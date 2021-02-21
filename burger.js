90ec523c9c69f8e1c9800e1fe2ccb113c5e60a18
purva1908 <72730647+purva1908@users.noreply.github.com>
2021-02-21 14:48:07 +0530

    Update burger.js

--- burger.js
+++ burger.js
@@ -1,1 +1,33 @@
 
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sample</title>
+</head>
+<body>
+
+<div id="a"> 
+     <div id="b"> 
+      <button>button</button>
+
+     </div>
+</div>	
+
+<script type="text/javascript">
+  
+let a1=document.querySelector("#a")
+let a2=document.querySelector("#b)
+a1.addEventListener("mousedown",event=>{
+	console.log("Outer div")
+});
+
+a2.addEventListener("mousedown",event=>{
+	console.log("inner div")
+});
+button.addEventListener("mousedown",event=>{
+	if(event.button==2) event.stopPropagation();
+});
+
+
+</body>
+</html>
